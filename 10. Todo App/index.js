3b7f54044cb50d569201b585c2a187d818173c0f
Andrew P <professor_smart@hotmail.com>
2022-06-23 22:54:10 +0000

    Added a todo app

---
+++ 10. Todo App/index.js
@@ -0,0 +1,31 @@
+const newTodoInput = document.getElementById("new-todo");
+const todos = document.getElementById("todos");
+
+newTodoInput.addEventListener("keypress", e => {
+    if (e.key != "Enter") return;
+    
+    let todo = e.target.value;
+
+    if (todo.trim() == "") return;
+
+    let todoElement = document.createElement("li");
+
+    let doneCheckbox = document.createElement("input");
+    doneCheckbox.classList.add("done-checkbox");
+    doneCheckbox.type = "checkbox";
+
+    doneCheckbox.addEventListener("input", e => {
+        todoElement.classList.toggle("done", e.target.checked);
+    });
+
+    todoElement.appendChild(doneCheckbox);
+
+    let existingTodo = document.createElement("input");
+    existingTodo.classList.add("existing-todo");
+    existingTodo.value = todo;
+
+    todoElement.appendChild(existingTodo);
+
+    if (todos.children.length == 0) todos.appendChild(todoElement);
+    else todos.insertBefore(todoElement, todos.firstElementChild);
+});
